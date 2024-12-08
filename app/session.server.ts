164e8e281c16cae5711e860201e8835a4d5f5f13
matteoltz <matteo.lutz06@outlook.de>
2024-12-08 21:50:53 +0100

    fixed cookies

--- app/session.server.ts
+++ app/session.server.ts
@@ -9,9 +9,9 @@ invariant(process.env.SESSION_SECRET, "SESSION_SECRET must be set");
 export const sessionStorage = createCookieSessionStorage({
   cookie: {
     name: "__session",
-    httpOnly: true,
+    httpOnly: false,
     path: "/",
-    sameSite: "lax",
+    sameSite: "none",
     secrets: [process.env.SESSION_SECRET],
     secure: process.env.NODE_ENV === "production",
   },
